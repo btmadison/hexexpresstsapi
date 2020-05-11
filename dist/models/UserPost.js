d107545c64e6b6843eeff8b15664637ffb347c1b
bmad <btmadison74@gmail.com>
2020-05-10 20:01:30 -0500

    jest test with jest mock of a ts class

--- dist/models/UserPost.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserPost {
-}
-exports.UserPost = UserPost;
-//# sourceMappingURL=UserPost.js.map
---
+++ src/services/users.spec.ts
@@ -0,0 +1,22 @@
+import { UsersService } from './users';
+import { UserInMemoryData } from '../data/user-inmem-repository';
+import { IUserRepository } from '../models/user/user-repository';
+
+jest.mock('../data/user-inmem-repository');
+
+describe('Users', () => {
+    let instance: UsersService;
+    let db: IUserRepository;
+
+    beforeEach(() => {
+        db = new UserInMemoryData();
+        instance = new UsersService(db);
+    });
+
+    it('should get all users as an array', async () => {
+        expect(instance).toBeInstanceOf(UsersService);
+        const allUsers = await instance.getAll();
+        expect(allUsers).toBeDefined();
+        expect(allUsers[0].id).toBe('MOCK_user-1');
+    });
+});
